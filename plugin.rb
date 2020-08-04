3c7ad679056f5341e6d40beb54d7d5341b721caa
Eddy Herman <eddy.herman@darktrace.com>
2020-08-04 11:04:24 +0100

    adding tracked support and rtline

--- plugin.rb
+++ plugin.rb
@@ -22,7 +22,7 @@ module ::WatchCategory
 
     #this is actually watching first post, not tracking
     groups_cats = {
-      'cybertechnician' => ['age', ['age', 'wiki'], 'eis', ['eis', 'wiki'], 'agn',['agn','wiki'],'saas',['saas','wiki'],'third-party', ['third-party','wiki'], 'iis',['iis','wiki'], ['internal', 'ctsad'],['internal','announcements'] ],
+      'cybertechnician' => ['age', ['age', 'wiki'], 'eis', ['eis', 'wiki'], 'agn',['agn','wiki'],'saas',['saas','wiki'],'third-party', ['third-party','wiki'], 'iis',['iis','wiki'], ['internal', 'ctsad'],['internal','announcements'],['internal', 'rtline'] ],
       'everyone' => [['internal','announcements'],'helios','age','eis','agn','saas','third-party','iis']
     }
 
@@ -49,9 +49,12 @@ module ::WatchCategory
               watched_categories = CategoryUser.lookup(user, pref).pluck(:category_id)
               muted_categories = CategoryUser.lookup(user, :muted).pluck(:category_id)
               watching_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+              tracking_categories = CategoryUser.lookup(user, :tracking).pluck(:category_id)
               if not muted_categories.include?(category.id)
                 if not watching_categories.include?(category.id)
+                  if not tracking_categories.include?(category.id)
               CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[pref], category.id) unless watched_categories.include?(category.id)
+                  end
                 end
               end
             end
@@ -60,9 +63,12 @@ module ::WatchCategory
               watched_categories = CategoryUser.lookup(user, pref).pluck(:category_id)
               muted_categories = CategoryUser.lookup(user, :muted).pluck(:category_id)
               watching_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+              tracking_categories = CategoryUser.lookup(user, :tracking).pluck(:category_id)
               if not muted_categories.include?(category.id)
                 if not watching_categories.include?(category.id)
+                  if not tracking_categories.include?(category.id)
               CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[pref], category.id) unless watched_categories.include?(category.id)
+                  end
                 end
               end
             end
